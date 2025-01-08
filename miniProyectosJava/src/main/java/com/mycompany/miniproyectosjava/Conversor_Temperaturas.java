8297b00aac43f9c1a566ef0ff5f6e86dcd3acae8
MaxwellRoyers <oriolfs2005@gmail.com>
2025-01-08 19:53:49 +0100

    Traduccion completada
    a

---
+++ miniProyectosJava/src/main/java/com/mycompany/miniproyectosjava/Conversor_Temperaturas.java
@@ -0,0 +1,25 @@
+package com.mycompany.miniproyectosjava;
+
+import java.util.Scanner;
+public class Conversor_Temperaturas {
+	public static void conversorTemp(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("\n\tBienvenido al Conversor de Temperaturas\n");
+	System.out.print("Introduce el valor que quieres convertir: ");
+	float value = scan.nextFloat();
+	System.out.println("  Introduce 1 para convertir Celsius a Fahrenheit.... ");
+	System.out.println("  Introduce 2 para convertir Fahrenheit a Celsius.... ");
+	System.out.print("\nIntroduce el valor aqui...");
+	int ans = scan.nextInt();
+	if (ans == 1) {
+		float value1 = (float) ((value*(1.8))+32);
+		System.out.print("La conversion es de.. "+value1+"�F");
+		}
+	if (ans == 2) {
+			float value2 = (float) (value-32);
+			float value3 = (float) (value2*(5)/9);
+			System.out.print("La conversion es de.. "+value3+"�C");
+		}
+	System.out.println("\n\t¡¡Gracias!!..");
+	}
+}
